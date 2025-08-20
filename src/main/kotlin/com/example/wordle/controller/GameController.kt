9c039b654f879aa6529a0e765272b768560cea27
Dinn <ib3smys@gmail.com>
2025-08-20 22:49:13 +0900

    GameService 추가

--- src/main/kotlin/com/example/wordle/controller/GameController.kt
+++ src/main/kotlin/com/example/wordle/controller/GameController.kt
@@ -1,15 +1,18 @@
 package com.example.wordle.controller
 
+import com.example.wordle.service.GameService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/game")
-class GameController {
+class GameController(
+    private val gameService: GameService
+) {
 
     @GetMapping("/new")
     fun getAnswer(): String {
-        return ""
+        return gameService.createNewGame()
     }
 }
---
+++ src/main/kotlin/com/example/wordle/service/GameService.kt
@@ -0,0 +1,11 @@
+package com.example.wordle.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class GameService {
+
+    fun createNewGame(): String {
+        return ""
+    }
+}
